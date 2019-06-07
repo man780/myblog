47582acdf7c3c09262091bb09d1ce2381726c64c
Murod Smith <man780@mail.ru>
2019-06-07 16:20:53 +0500

    Created templates for views (list, detail)

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-# TODO
-class PublishedManager(models.Model):
+
+class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -21,14 +23,19 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() #Our custome manager.
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
+
+    def __str__(self):
+        return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
 
     class Meta:
         ordering = ('-publish',)
-
-        def __str__(self):
-            return self.title
