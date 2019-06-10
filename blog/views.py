656c9b36f928907d216571ce80778234bc1b3eb1
Murod Smith <man780@mail.ru>
2019-06-10 11:42:09 +0500

    Render forms

--- blog/views.py
+++ blog/views.py
@@ -42,22 +42,24 @@ class PostListView(ListView):
 def post_share(request, post_id):
     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
 
     if request.method == 'POST':
-        # Form was submited
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
             post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
             message = 'Read "{}" at {} \n\n{}\s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
             send_mail(subject, message, 'blogermurod@gmail.com', [cd['to']])
             sent = True
-        else:
-            form = EmailPostForm()
+    else:
+        form = EmailPostForm()
 
-        return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+        return render(request, 'blog/post/share.html', {'post': post,
+                                                        'form': form,
+                                                        'sent': sent})
 
 
