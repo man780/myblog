10f571aabb72716333b8d2d534c38e7d68bb1ad7
Murod Smith <man780@mail.ru>
2019-06-13 12:44:07 +0500

    Created feeds

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog'
@@ -10,5 +11,6 @@ urlpatterns = [
     #path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
-    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag')
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
