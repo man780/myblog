5402a44de94206a89560e17aafc250b1b8419f93
Murod Smith <man780@mail.ru>
2019-08-18 22:47:50 +0500

    Requirements file created and url corrected

--- myblog/urls.py
+++ myblog/urls.py
@@ -24,6 +24,7 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls', namespace='blog')),
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
